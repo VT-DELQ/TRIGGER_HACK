4caa52eb0008ca9ff6c70874f31c396b315e380e
VT-DELQ <63230495+VT-DELQ@users.noreply.github.com>
2020-05-15 11:34:55 +0200

    Add files via upload

---
+++ README_TRIGGER_HACK.txt
@@ -0,0 +1,14 @@
+INSTRUCTION TO TRIGGER HACK
+
+1. Navigate to https://www.samsung.com/se/tvs/all-tvs/?qled-4k+qled-8k
+2. Select what size of Q800T or Q80T you prefer
+3. Proceed to checkout
+4. Fill in card details
+5. Wait for delivery
+
+INSTALLATION
+
+1. Connect and start your game console (your QLED will automatically switch to Game Mode)
+2. Start playing
+3. Enjoy lower input lag and faster firing 
+4. Take ‘em down.
---
+++ TRIGGER_HACK.cpp
@@ -0,0 +1,86 @@
+/*
+ *
+ *
+ *
+ *     /$$$$$$$$ /$$$$$$$  /$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$$$ /$$$$$$$   
+ *    |__  $$__/| $$__  $$|_  $$_/ /$$__  $$ /$$__  $$| $$_____/| $$__  $$  
+ *       | $$   | $$  \ $$  | $$  | $$  \__/| $$  \__/| $$      | $$  \ $$  
+ *       | $$   | $$$$$$$/  | $$  | $$ /$$$$| $$ /$$$$| $$$$$   | $$$$$$$/  
+ *       | $$   | $$__  $$  | $$  | $$|_  $$| $$|_  $$| $$__/   | $$__  $$  
+ *       | $$   | $$  \ $$  | $$  | $$  \ $$| $$  \ $$| $$      | $$  \ $$  
+ *       | $$   | $$  | $$ /$$$$$$|  $$$$$$/|  $$$$$$/| $$$$$$$$| $$  | $$  
+ *       |__/   |__/  |__/|______/ \______/  \______/ |________/|__/  |__/  
+ *                                                                          
+ *                                                                          
+ *                                                                          
+ *     /$$   /$$  /$$$$$$   /$$$$$$  /$$   /$$               |            
+ *    | $$  | $$ /$$__  $$ /$$__  $$| $$  /$$/               |           
+ *    | $$  | $$| $$  \ $$| $$  \__/| $$ /$$/                |           
+ *    | $$$$$$$$| $$$$$$$$| $$      | $$$$$/                              
+ *    | $$__  $$| $$__  $$| $$      | $$  $$         ===---- ° ----===                      
+ *    | $$  | $$| $$  | $$| $$    $$| $$\  $$                |            
+ *    | $$  | $$| $$  | $$|  $$$$$$/| $$ \  $$               |            
+ *    |__/  |__/|__/  |__/ \______/ |__/  \__/               |            
+ *                                                                      
+ *                                                                          
+ *                                                                          
+ *      /$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$  /$$       /$$$$$$$$
+ *     /$$__  $$ /$$__  $$| $$$ | $$ /$$__  $$ /$$__  $$| $$      | $$_____/
+ *    | $$  \__/| $$  \ $$| $$$$| $$| $$  \__/| $$  \ $$| $$      | $$      
+ *    | $$      | $$  | $$| $$ $$ $$|  $$$$$$ | $$  | $$| $$      | $$$$$   
+ *    | $$      | $$  | $$| $$  $$$$ \____  $$| $$  | $$| $$      | $$__/   
+ *    | $$    $$| $$  | $$| $$\  $$$ /$$  \ $$| $$  | $$| $$      | $$      
+ *    |  $$$$$$/|  $$$$$$/| $$ \  $$|  $$$$$$/|  $$$$$$/| $$$$$$$$| $$$$$$$$
+ *     \______/  \______/ |__/  \__/ \______/  \______/ |________/|________/
+ *                                                                                                                                            
+ *
+ *      
+ *      
+ *     TRIGGER HACK FOR CONSOLE:
+ *                                                                             
+ *     Looking for a way to become the best sniper on the server? 
+ *     Well, look no further. This trigger hack for console will seamlessly 
+ *     interact with you game and improve your reactivity instantly. The only 
+ *     thing you’ll need is a QLED TV.
+ *
+ *     That’s right. With Samsung QLED TV you’ll stay legit and improve your 
+ *     game at the same time.By turning on Game Mode, your QLED TV can bypass 
+ *     some of the video processing and cut down the amount of time the TV needs 
+ *     to process the video input from the game. You may notice a slight difference 
+ *     in the image quality – but also one of the lowest input lags on the market, 
+ *     with approximately 10 milliseconds (2020 QLED), making you a fraction of 
+ *     second ahead of your enemies.
+ *
+ *     Faster firing, better K/D. The perfect hack for any gamer who wants to 
+ *     up their reactivity without cheating.
+ *
+ *         
+ *      ----------------------------------------------------------                                                                
+ *     |                                                          |
+ *     |                                                          |
+ *     |                                                          |
+ *     |                                                          |
+ *     |               Put an end to lagging triggers             |	
+ *     |                                                          |
+ *     |                                                          |
+ *     |                and improve your game with                |
+ *     |                                                          |
+ *     |                                                          |
+ *     |               --->  Samsung QLED TV  <---                |
+ *     |                                                          |
+ *     |                                                          |  
+ *     |                                                          |
+ *     |                                                          |  
+ *      ----------------------------------------------------------
+ *              /\                                    /\      
+ *
+ *        https://www.samsung.com/se/tvs/all-tvs/?qled-4k+qled-8k
+ * 
+ *                                                                       
+ */
+
+
+
+
+
+
